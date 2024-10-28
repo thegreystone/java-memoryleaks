5c5eb0391cd09f271e29d7f79eb7d897fc4aa72a
Marcus Hirt <marcus@hirt.se>
2024-10-28 22:38:28 +0100

    Thread leak (#4)
    Adding a thread leak option

--- src/main/java/se/hirt/memoryleaks/NativeLeaks.java
+++ src/main/java/se/hirt/memoryleaks/NativeLeaks.java
@@ -3,8 +3,11 @@ package se.hirt.memoryleaks;
 public class NativeLeaks {
 	/**
 	 * Will leak a specified number of global handles.
-	 * @param count the number of handles to leak.
+	 *
+	 * @param count
+	 * 		the number of handles to leak.
 	 */
 	public static native void leakGlobalHandle(int count);
+
 	public static native void leakMallocedMemory(int count);
 }
--- src/main/java/se/hirt/memoryleaks/RunLeak.java
+++ src/main/java/se/hirt/memoryleaks/RunLeak.java
@@ -1,55 +1,70 @@
 package se.hirt.memoryleaks;
 
 public class RunLeak {
-    private final static String LIBRARY_NAME = "memleak";
+	private final static String LIBRARY_NAME = "memleak";
 
-    static {
-        NativeUtils.loadLibrary(LIBRARY_NAME);
-    }
+	static {
+		NativeUtils.loadLibrary(LIBRARY_NAME);
+	}
 
-    private static native void sayHello();
+	private static native void sayHello();
 
-    public static void main(String[] args) throws InterruptedException {
-        LeakConfiguration config = LeakConfiguration.parseArguments(args);
-        RunLeak.sayHello();
+	public static void main(String[] args) throws InterruptedException {
+		LeakConfiguration config = LeakConfiguration.parseArguments(args);
+		RunLeak.sayHello();
 
-        switch (config.getLeakType()) {
-            case NATIVE_GLOBAL_HANDLE:
-                runNativeGlobalHandleLeak(config.getLeaksPerBatch(), config.getPauseInMsBetweenBatches(), config.getTotalNumberOfBatches());
-                break;
-            case NATIVE_MALLOC:
-                runNativeMallocLeak(config.getLeaksPerBatch(), config.getPauseInMsBetweenBatches(), config.getTotalNumberOfBatches(), config.getBytesPerLeak());
-                break;
-            case HEAP:
-                runHeapLeak(config.getLeaksPerBatch(), config.getPauseInMsBetweenBatches(), config.getTotalNumberOfBatches());
-                break;
-        }
-    }
+		switch (config.getLeakType()) {
+		case NATIVE_GLOBAL_HANDLE:
+			runNativeGlobalHandleLeak(config.getLeaksPerBatch(), config.getPauseInMsBetweenBatches(), config.getTotalNumberOfBatches());
+			break;
+		case NATIVE_MALLOC:
+			runNativeMallocLeak(config.getLeaksPerBatch(), config.getPauseInMsBetweenBatches(), config.getTotalNumberOfBatches(),
+					config.getBytesPerLeak());
+			break;
+		case HEAP:
+			runHeapLeak(config.getLeaksPerBatch(), config.getPauseInMsBetweenBatches(), config.getTotalNumberOfBatches());
+			break;
+		case THREAD_LEAK:
+			runThreadLeak(config.getLeaksPerBatch(), config.getPauseInMsBetweenBatches(), config.getTotalNumberOfBatches());
+			break;
+		}
+	}
 
-    public static void runNativeGlobalHandleLeak(int leaksPerBatch, int pauseInMsBetweenBatches, int totalNumberOfBatches) throws InterruptedException {
-        for (int i = 0; i < totalNumberOfBatches; i++) {
-            NativeLeaks.leakGlobalHandle(leaksPerBatch);
-            Thread.sleep(pauseInMsBetweenBatches);
-            System.out.println("Total leaked objects " + (i + 1) * leaksPerBatch);
-        }
-    }
+	public static void runNativeGlobalHandleLeak(int leaksPerBatch, int pauseInMsBetweenBatches, int totalNumberOfBatches)
+			throws InterruptedException {
+		for (int i = 0; i < totalNumberOfBatches; i++) {
+			NativeLeaks.leakGlobalHandle(leaksPerBatch);
+			Thread.sleep(pauseInMsBetweenBatches);
+			System.out.println("Total leaked objects " + (i + 1) * leaksPerBatch);
+		}
+	}
 
-    public static void runHeapLeak(int leaksPerBatch, int pauseInMsBetweenBatches, int totalNumberOfBatches) throws InterruptedException {
-        MapLeak leak = new MapLeak(leaksPerBatch, leaksPerBatch * 3);
-        for (int i = 0; i < totalNumberOfBatches; i++) {
-            leak.runBatch();
-            Thread.sleep(pauseInMsBetweenBatches);
-            System.out.println("Total leaked map entries " + (i + 1) * leaksPerBatch);
-        }
-    }
+	public static void runHeapLeak(int leaksPerBatch, int pauseInMsBetweenBatches, int totalNumberOfBatches) throws InterruptedException {
+		MapLeak leak = new MapLeak(leaksPerBatch, leaksPerBatch * 3);
+		for (int i = 0; i < totalNumberOfBatches; i++) {
+			leak.runBatch();
+			Thread.sleep(pauseInMsBetweenBatches);
+			System.out.println("Total leaked map entries " + (i + 1) * leaksPerBatch);
+		}
+	}
 
-    public static void runNativeMallocLeak(int leaksPerBatch, int pauseInMsBetweenBatches, int totalNumberOfBatches, int bytesPerLeak) throws InterruptedException {
-        for (int i = 0; i < totalNumberOfBatches; i++) {
-            for (int j = 0; j < leaksPerBatch; j++) {
-                NativeLeaks.leakMallocedMemory(bytesPerLeak);
-            }
-            Thread.sleep(pauseInMsBetweenBatches);
-            System.out.println("Total leaked memory " + ((i + 1) * leaksPerBatch * bytesPerLeak) + " bytes");
-        }
-    }
+	public static void runNativeMallocLeak(int leaksPerBatch, int pauseInMsBetweenBatches, int totalNumberOfBatches, int bytesPerLeak)
+			throws InterruptedException {
+		for (int i = 0; i < totalNumberOfBatches; i++) {
+			for (int j = 0; j < leaksPerBatch; j++) {
+				NativeLeaks.leakMallocedMemory(bytesPerLeak);
+			}
+			Thread.sleep(pauseInMsBetweenBatches);
+			System.out.println("Total leaked memory " + ((i + 1) * leaksPerBatch * bytesPerLeak) + " bytes");
+		}
+	}
+
+	public static void runThreadLeak(int threadsPerBatch, int pauseInMsBetweenBatches, int totalNumberOfBatches)
+			throws InterruptedException {
+		for (int i = 0; i < totalNumberOfBatches; i++) {
+			ThreadLeaks.leakThreads(threadsPerBatch);
+			Thread.sleep(pauseInMsBetweenBatches);
+			System.out.println("Total leaked threads " + (i + 1) * threadsPerBatch);
+		}
+	}
 }
